504a93ebb3fd798ed23512819cabeef5779b0b1d
IvanovaNataly <letter4natalia@gmail.com>
2017-11-12 09:12:36 +0200

    wobbling icon to open side bar

---
+++ wobble.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tresermichael on 11/12/17.
+ */
+function toggleSidebar() {
+	var btnDefinitions = document.querySelector(".c_btnDefinitions");
+	// document.querySelector(".c_sideFrame").classList.toggle("c_widthNull");
+	// document.querySelector(".c_mainFrame").classList.toggle("c_widthFull");
+	btnDefinitions.classList.toggle("c_btnRotate");
+}
